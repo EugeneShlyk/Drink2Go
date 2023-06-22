c7f96b39bcb5466461fb0387bf115973a96924ad
Evgene Shlyk <serpupstars@gmail.com>
2023-06-22 23:51:51 +1000

    первоначальное завершение

--- source/js/main.js
+++ source/js/main.js
@@ -1,8 +1,20 @@
-import { toggleMenuHandler } from './menu.js'
+import { toggleMenuHandler } from './menu.js';
 import { addNoUiSlider } from './range-slider.js';
 
 toggleMenuHandler();
 addNoUiSlider();
 
+new Swiper('.image-slider', {
 
+  navigation: {
+    nextEl: '.image-slider__button-next',
+    prevEl: '.image-slider__button-prev'
+  },
 
+  pagination: {
+    el: '.swiper-pagination',
+
+    clickable: true,
+
+  }
+});
